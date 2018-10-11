ed59da0c4f2fa44dc49cb4abfd063b4219e80b84
Adam Seal <aseal33@gmail.com>
2018-10-11 11:59:08 -0400

    Just Getting Started

---
+++ Capstone/Classes/IDispensible.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public interface IDispensible
+    {
+        string Name { get; }
+
+        string Location { get; }
+
+        decimal Price { get; }
+
+        string Type { get; }
+
+        string Consumed(Type type);        
+    }
+}
--- Capstone/Program.cs
+++ Capstone/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Capstone
 {
@@ -6,7 +7,53 @@ namespace Capstone
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal balance = 0;
+            bool run = true;
+
+            while (run)
+            {
+                Console.WriteLine("> Please Enter a Whole Dollar Amount(1, 2, 5, 10) or (q)uit: ");
+                string input = Console.ReadLine();
+
+
+                if (input == "1" || input == "2" || input == "5" || input == "10")
+                {
+                    int amount = int.Parse(input);
+                    balance += (decimal)amount;
+
+                    Console.WriteLine($"${input} inserted. Current Balance is ${balance}");
+                }
+                else if (input == "q")
+                {
+                    decimal totalBalance = balance;
+                    int numQuarter = 0;
+                    int numDime = 0;
+                    int numNickel = 0;
+
+                    while (balance > 0)
+                    {
+                        if (balance >= 0.25M)
+                        {
+                            numQuarter++;
+                            balance -= 0.25M;
+                        }
+                        else if (balance >= 0.10M)
+                        {
+                            numDime++;
+                            balance -= 0.10M;
+                        }
+                        else if (balance >= 0.50M)
+                        {
+                            numNickel++;
+                            balance -= 0.05M;
+                        }
+                    }
+
+                    Console.Write($"Here is your change: {numQuarter} Quarter(s), {numDime} Dime(s), {numNickel} Nickel(s).");
+
+                    run = false;
+                }
+            }
         }
     }
 }
