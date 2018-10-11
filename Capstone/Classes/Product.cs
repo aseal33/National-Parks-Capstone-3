5c7eb3984eedc04d8f294d9dc6231cc26385ee46
Adam Seal <aseal33@gmail.com>
2018-10-11 15:57:14 -0400

    Pass to you

---
+++ Capstone/Classes/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public class Product
+    {
+        public string Name { get; }
+        public string Location { get; }
+        public decimal Price { get; }
+        public string Type { get; }
+
+        public Product(string name, string location, decimal price, string type)
+        {
+            this.Name = name;
+            this.Location = location;
+            this.Price = price;
+            this.Type = type;
+        }
+    }
+}
--- Capstone/Classes/VendingMachine.cs
+++ Capstone/Classes/VendingMachine.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Capstone.Classes
 {
     public class VendingMachine
     {
-        
+        public VendingMachine()
+        {
+            List<Product> products = new List<Product>();
+
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] separated = line.Split("|");
+
+                    
+                    Product product = new Product(separated[1], separated[0], decimal.Parse(separated[2]), separated[3]);
+                    products.Add(product);
+                }
+            }
+        }
     }
 }
